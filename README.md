7c63810b19f17fbeb6240c0c551fa6999fc6282b
Kirtan Patel <kirtan.patel@zymr.com>
2021-06-17 12:51:09 +0530

    Adding TOTP Code

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# insomnia-plugin-totp-generator
-insomnia-plugin-totp-generator
+
+## How to Install
+---
+1. Go to Insomnia/Preferences and choose the Plugins tab
+2. Search insomnia-plugin-totp-generator and Install  
+  
---
+++ main.js
@@ -0,0 +1,28 @@
+const totp = require("totp-generator");
+
+module.exports.templateTags = [
+  {
+    name: "TOTP Generator",
+    displayName: "TOTP Generator",
+    description: "Generate TOTP Code",
+    args: [
+      {
+        displayName: "MFA Secret",
+        description: "MFA Secret",
+        type: "string",
+        defaultValue: "YOUR_MFA_SECRET",
+      },
+      {
+        displayName: "Period",
+        description: "Period",
+        type: "number",
+        defaultValue: 60,
+      },
+    ],
+    async run(context, mfaSecret, period) {
+      const token = totp(mfaSecret, { period: period });
+      console.log(token);
+      return token;
+    },
+  },
+];
